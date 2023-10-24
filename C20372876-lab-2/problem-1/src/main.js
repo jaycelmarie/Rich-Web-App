16c57087a6059bbfb2786c5ce1c2d09aa5939dda
jaycelmarie <C20372876@mytudublin.ie>
2023-10-24 20:47:24 +0100

    Problem 1 completed

--- C20372876-lab-2/problem-1/src/main.js
+++ C20372876-lab-2/problem-1/src/main.js
@@ -1,7 +1,3 @@
-//check if the number is odd
-function isOdd (number) {
-  return number % 2 !== 0;
-}
 
 class Phone {
     constructor(name, num, email) {
@@ -92,7 +88,7 @@ document.querySelector('#num-form').addEventListener('submit', (e) => {
   } else if(name.length >= 20) {
       phoneDirectory.Alert("name-error");
       phoneDirectory.clearFields();
-  } else if(isNaN(num)){
+  } else if(isNaN(num) || num.length < 10){
       phoneDirectory.Alert("num-error");
       phoneDirectory.clearFields();
   } else if(email.length >= 40) {
@@ -113,16 +109,6 @@ document.querySelector('#num-form').addEventListener('submit', (e) => {
     phoneDirectory.Alert("email-error");
     phoneDirectory.clearFields();
   }
-});
-
-document.querySelector('#Plist').addEventListener('click', (e) => {
-  phoneDirectory.deleteBook(e.target);
-
-  Directory.removeBook(e.target.parentElement.previousElementSibling.textContent);
-  
-  phoneDirectory.Alert("remove");
-
-  
 });
 
 
@@ -154,8 +140,8 @@ function sortTable(n) {
             shouldSwitch = false;
 
             // Get two elements to compare, from curr row to next row
-            x = rows[i].getElementsByTagName("TD")[n];
-            y = rows[i + 1].getElementsByTagName("TD")[n];
+            x = rows[i].getElementsByTagName("td")[n];
+            y = rows[i + 1].getElementsByTagName("td")[n];
 
             // Check if two rows should switch, based on asc or desc
             if (dir == "asc") {
@@ -184,81 +170,40 @@ function sortTable(n) {
     } // End while loop
 } // End func
 
-// Clear fields
-function clearFields() {
-
-  // if (document.querySelector('#tbl').length == 0) {
-  //     console.log("Table is empty")
-
-  // } else {
-  //     const list = document.getElementById('Plist');
-  //     list.remove();
-  // }
-
-  var x = document.getElementById('tbl');
-
-  if (x.style.display === "none") {
-      x.style.display = "block";
-
-  } else {
-      x.style.display = "none";
-      const list = document.getElementById('Plist');
-      list.remove();
-       
-      // console.log("Table empty");
-  }
- 
-  // if (document.getElementById('tbl').indexOf(book.num).value > -1) {
-  //   x[i].style.display = "";
-  // } else {
-  //     x[i].style.display = "none";
-  // }
-}
-
 /* Function to Search for Phone Numbers */
 function search() {
+  var input, filter, tbl, tr, td, i, txtValue;
+  input = document.getElementById("num2");
+  filter = input.value.toUpperCase();
+  tbl = document.getElementById("tbl");
+  tr = tbl.getElementsByTagName("tr");
 
-  clearFields();
-
-  const inputNum = document.querySelector('#num2').value;
-  const table = document.querySelector('#tbl');
-  const row = document.createElement('tbody');
-
-  row.setAttribute('id', 'list');
-
-  let books = Directory.getBooks();
-  let searched = false;
-
-  // Search number 
-  books.forEach(book => {
-      
-      if((book.num).includes(inputNum)) {
-
-          if (isOdd(book.num)) {
-              row.innerHTML += `
-                  <tr>
-                      <td style="background-color: #f2f2f2">${book.name}</td>
-                      <td style="background-color: #f2f2f2">${book.num}</td>
-                      <td style="background-color: #f2f2f2">${book.email}</td>
-                  </tr>
-              `;
-          } else {
-              row.innerHTML += `
-              <tr>
-                  <td>${book.name}</td>
-                  <td>${book.num}</td>
-                  <td>${book.email}</td>
-              </tr>
-              `;
-          }
-          table.appendChild(row);
-
-          searched = true;
-      }
-
-  });
+  var counter = tr.length-1;
   
-  if (!searched) {
-    phoneDirectory.Alert("no-result");
+  // displays no result 
+  let noRes = document.getElementById("no-result");
+  
+  for(i = 0; i < tr.length; i++) {
+
+    td = tr[i].getElementsByTagName("td")[1];
+
+    // if there's data content
+    if(td) {
+      txtValue = td.textContent || td.innerText;
+      
+      if(txtValue.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.display = "";
+      }
+      else {
+        tr[i].style.display = "none";
+        counter = counter-1;
+      }
+    }
+  }
+  // No results from search
+  if(counter == 0){ 
+    noRes.style.display="block";
+  } else{
+    noRes.style.display = "none";
   }
 }
