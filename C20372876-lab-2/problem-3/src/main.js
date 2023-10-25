b42f5ca5bafac6c433916697e241d75093333526
jaycelmarie <C20372876@mytudublin.ie>
2023-10-25 23:35:20 +0100

    CSS changes to make two tables

--- C20372876-lab-2/problem-3/src/main.js
+++ C20372876-lab-2/problem-3/src/main.js
@@ -7,20 +7,32 @@ form.addEventListener('submit', function(e){
 
     var origName = search.split(' ').join('')
 
-
     fetch("https://api.github.com/users/"+origName)
     .then((result) => result.json())
     .then((data) => {
         console.log(data)
 
-        document.getElementById("result").innerHTML = `
-            <img src="${data.avatar_url}"/>
+        document.getElementById("result").innerHTML += `
+            <img src="${data.avatar_url}"/> 
             <tr>
-            <th>Name</th><td>${data.name ? data.name : "N/A"}</td>
-            <th>Username</th><td>${data.login ? data.login : "N/A"}</td>
-            <th>Email</th><td>${data.email ? data.email : "N/A"}</td>
-            <th>Location</th><td>${data.location ? data.location : "N/A"}</td>
-            <th>Number of Gists</th><td>${data.public_gists ? data.public_gists : "N/A"}</td>
+                <th>Name: </th>
+                <td>${data.name ? data.name : "N/A"}</td>
+            </tr>
+            <tr>
+                <th>Username: </th>
+                <td>${data.login ? data.login : "N/A"}</td>
+            </tr>
+            <tr>
+                <th>Email: </th>
+                <td>${data.email ? data.email : "N/A"}</td>
+            </tr>
+            <tr>
+                <th>Location: </th>
+                <td>${data.location ? data.location : "N/A"}</td>
+            </tr>
+            <tr>
+                <th>Number of Gists: </th>
+                <td>${data.public_gists ? data.public_gists : "N/A"}</td>
             </tr>
         `
     })
@@ -33,8 +45,14 @@ form.addEventListener('submit', function(e){
 
         for(let i = 0; i < data.length; i++) {
             document.getElementById("result2").innerHTML += `
-            <td>${data[i].name}</td>
-            <td>${data[i].description ? data[i].description : "N/A"}</td>
+            <tr>
+                <th>Title </th>
+                <td>${data[i].name}</td>
+            </tr>
+            <tr>
+                <th>Description</th>
+                <td>${data[i].description ? data[i].description : "N/A"}</td>
+            </tr>
             `
         }
         
