04267ad8ac2269422a4b1ca3a02a66dab0c9a25a
jaycelmarie <C20372876@mytudublin.ie>
2023-11-25 22:23:13 +0000

    Added createNote rxjs + edited html

--- C20372876-lab-3/problem-3/src/main.js
+++ C20372876-lab-3/problem-3/src/main.js
@@ -1,3 +1,57 @@
 document.addEventListener("DOMContentLoaded", function() {
+    const { Subject } = rxjs;
 
+    class NoteList {
+      constructor(id, content) {
+        this.id = id;
+        this.content = content;
+        this.children = [];
+        this.parentSubject = new Subject();
+      }
+
+      addChild(childNote) {
+        this.children.push(childNote);
+      }
+
+      setParent(parentNote) {
+        this.parentSubject.next(parentNote);
+      }
+
+      deleteNote() {
+        this.parentSubject.next(); // Notify parent to delete this note from its children
+
+        // Recursively delete all child notes
+        this.children.forEach(child => child.deleteNote());
+      }
+    } // End NoteList
+
+    const notes = []; // Array to store created notes
+
+    window.createNote = function() {
+      const contentInput = document.getElementById('content');
+      const parentSelect = document.getElementById('parentNote');
+      const content = contentInput.value;
+      const parentId = parentSelect.value;
+
+      if (content) {
+        let parentNote;
+
+        if (parentId) {
+          parentNote = notes.find(note => note.id === parseInt(parentId));
+        }
+
+        const newNote = new NoteList(Date.now(), content);
+
+        if (parentNote) {
+          parentNote.addChild(newNote);
+        } else {
+          notes.push(newNote);
+        }
+
+        // Clear the input fields
+        contentInput.value = '';
+
+
+      }
+    }; // End createNote()
 });
