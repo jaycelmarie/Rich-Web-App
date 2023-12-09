4fde0371497de6d63c02c7767876716c4a0085c7
jaycelmarie <C20372876@mytudublin.ie>
2023-12-09 22:25:56 +0000

    Added Cat API fact to notes

--- C20372876-lab-4/src/Components/NotesApp.js
+++ C20372876-lab-4/src/Components/NotesApp.js
@@ -1,10 +1,11 @@
 import React, { useState, useEffect, useContext } from 'react';
 import { LoginContext } from "../Contexts/LoginContext";
 import '../main.css';
+import Axios from 'axios';
 
 function NotesApp() {
   const [notes, setNotes] = useState([]);
-  const { username, setUsername, setShowProfile } = useContext(LoginContext);
+  const { username, setShowProfile } = useContext(LoginContext);
 
   useEffect(() => {
     // Load notes from local storage 
@@ -14,10 +15,10 @@ function NotesApp() {
     }
   }, []);
 
-  const addNote = (color) => {
+  const addNote = (content, color) => {
     const newNote = {
       id: Math.floor(Math.random() * 100000),
-      content: '',
+      content: content,
       color: color,
     };
 
@@ -50,107 +51,131 @@ function NotesApp() {
     localStorage.setItem('notetaking-notes', JSON.stringify(updatedNotes));
   };
 
+  // Cat Fact API
+  const catFact = async () => {
+    try {
+      const response = await Axios.get('https://catfact.ninja/fact', {
+        headers: {
+          'Content-Type': 'application/json'
+        },
+      });
+      
+      const data = response.data;
+
+      if (data.fact) {
+        addNote(data.fact, '#EE82EE'); // Assuming color for cat fact notes
+      }
+    } catch (error) {
+      console.error('Error fetching Cat fact:', error.message);
+    }
+  };
+
   return (
     <>
-          {/* Show Profile */}
-          <h1> Profile </h1>
-          <h2> Welcome {username} </h2>
+      {/* Show Profile */}
+      <h1> Profile </h1>
+      <h2> Welcome {username} </h2>
 
-          <button id="loginbtn" onClick={() => {setShowProfile(false)}}>LOGOUT</button>
+      <button id="loginbtn" onClick={() => setShowProfile(false)}>
+        LOGOUT
+      </button>
+      <button onClick={catFact}>Cat Fact!</button>
 
-          <div id="note-app">
-              {notes.map((note) => (
-                  <Note
-                      key={note.id}
-                      id={note.id}
-                      content={note.content}
-                      color={note.color}
-                      updateNote={updateNote}
-                      deleteNote={deleteNote} />
-              ))}
-              {/* Save button */}
-              <button className="save-note" type="button">
-                  <img className="save-btn" src="save-btn.png" alt="Save" />
-              </button>
-              {/* Colours for notes */}
-              <div
+      <div id="note-app">
+        {notes.map((note) => (
+          <Note
+            key={note.id}
+            id={note.id}
+            content={note.content}
+            color={note.color}
+            updateNote={updateNote}
+            deleteNote={deleteNote}
+          />
+        ))}
+        {/* Save button */}
+        <button className="save-note" type="button">
+          <img className="save-btn" src="save-btn.png" alt="Save" />
+        </button>
+        {/* Colours for notes */}
+        <div
                   className="add-note"
-                  onClick={() => addNote('#1E90FF')}
+                  onClick={() => addNote(' ', '#1E90FF')}
                   style={{ backgroundColor: '#1E90FF' }}
               >
                   +
               </div>
               <div
                   className="add-note"
-                  onClick={() => addNote('#5F9EA0')}
+                  onClick={() => addNote(' ', '#5F9EA0')}
                   style={{ backgroundColor: '#5F9EA0' }}
               >
                   +
               </div>
               <div
                   className="add-note"
-                  onClick={() => addNote('#F0E68C')}
+                  onClick={() => addNote(' ', '#F0E68C')}
                   style={{ backgroundColor: '#F0E68C' }}
               >
                   +
               </div>
               <div
                   className="add-note"
-                  onClick={() => addNote('#F0E68C')}
+                  onClick={() => addNote(' ', '#F0E68C')}
                   style={{ backgroundColor: '#F0E68C' }}
               >
                   +
               </div>
               <div
                   className="add-note"
-                  onClick={() => addNote('#ADFF2F')}
+                  onClick={() => addNote(' ', '#ADFF2F')}
                   style={{ backgroundColor: '#ADFF2F' }}
               >
                   +
               </div>
               <div
                   className="add-note"
-                  onClick={() => addNote('#DC143C')}
+                  onClick={() => addNote(' ', '#DC143C')}
                   style={{ backgroundColor: '#DC143C' }}
               >
                   +
               </div>
               <div
                   className="add-note"
-                  onClick={() => addNote('#EE82EE')}
+                  onClick={() => addNote(' ', '#EE82EE')}
                   style={{ backgroundColor: '#EE82EE' }}
               >
                   +
               </div>
               <div
                   className="add-note"
-                  onClick={() => addNote('#8A2BE2')}
+                  onClick={() => addNote(' ', '#8A2BE2')}
                   style={{ backgroundColor: '#8A2BE2' }}
               >
                   +
               </div>
               <div
                   className="add-note"
-                  onClick={() => addNote('#2F4F4F')}
+                  onClick={() => addNote(' ', '#2F4F4F')}
                   style={{ backgroundColor: '#2F4F4F' }}
               >
                   +
               </div>
               <div
                   className="add-note"
-                  onClick={() => addNote('#696969')}
+                  onClick={() => addNote(' ', '#696969')}
                   style={{ backgroundColor: '#696969' }}
               >
                   +
               </div>
               <div
                   className="add-note"
-                  onClick={() => addNote('#D3D3D3')}
+                  onClick={() => addNote(' ', '#D3D3D3')}
                   style={{ backgroundColor: '#D3D3D3' }}
               >
                   +
               </div>
-          </div></>
+          </div>
+    </>
   );
 } // End of NotesApp
 
